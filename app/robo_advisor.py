15fbbd583162427ea885b389348984e747d63e6f
Preetibhatia <50919836+Preetibhatia@users.noreply.github.com>
2019-06-22 13:17:04 -0400

    added write to csv functionality

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -6,25 +6,19 @@ import datetime
 import csv
 from dotenv import load_dotenv
 import os
+#import statistics
+import pandas as df
 load_dotenv()
+
+
 
 API_KEY = os.environ["ALPHAVANTAGE_API_KEY"]
 
-#implementing user input validation logic
-# check = False
-# while check==False:
-#     input_symbol = input("Enter Stock symbol, e.g. 'MSFT, AAPL, GOOG, AMZN':  ")
-#     if input_symbol not in ('MSFT, AAPL, GOOG, AMZN, AXP'):
-#         print("Oh, expecting a properly-formed stock symbol like 'MSFT'. Please try again.")
-#         check = False
-#     else:
-#         check = True
-
 #using requests package to access the API
-####if symbol not found return msg "Sorry, couldn't find any trading data for that stock symbol" and exit prog 
+####if symbol numeric/inappropriate length prompt "Oh, expecting a properly-formed stock symbol like 'MSFT'. Please try again." 
 def get_response(symbol):
     
-    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={input_symbol}&outputsize=full&apikey={API_KEY}"
+    request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={input_symbol}&outputsize=compact&apikey={API_KEY}"
     response = requests.get(request_url)
     parsed_response = json.loads(response.text)
     if "Error Message" in parsed_response:
@@ -34,10 +28,9 @@ def get_response(symbol):
 
 #1998-12-23': {'1. open': '140.3800', '2. high': '143.8100', '3. low': '139.3800', '4. close': '143.5600', '5. volume': '8735000'}
 def transform_response(parsed_response):
-        tsd=parsed_response["Time Series (Daily)"]
-        
+        x=parsed_response["Time Series (Daily)"]
         rows = []
-        for date, daily_prices in tsd.items(): 
+        for date, daily_prices in x.items(): 
             row = {
                 "timestamp": date,
                 "open": float(daily_prices["1. open"]),
@@ -49,6 +42,19 @@ def transform_response(parsed_response):
             rows.append(row)
         return rows
 
+def write_to_csv(rows, csa_filepath):
+    csv_headers = ["timestamp", "open", "high", "low", "close", "volume"]
+    with open(csv_filepath, "w") as csv_file:
+        writer = csv.DictWriter(csv_file, fieldnames=csv_headers)
+        writer.writeheader() # uses fieldnames set above
+        for row in tsd:
+            writer.writerow(row)
+    return True
+
+def to_usd(my_price):
+    # utility function to convert float or integer to usd-formatted string (for printing)
+    return "${0:,.2f}".format(my_price) #> $12,000.71
+
 #main program
 if __name__ == "__main__":
     time_now = datetime.datetime.now() 
@@ -56,29 +62,26 @@ if __name__ == "__main__":
     parsed_response = get_response(input_symbol)
     tsd = transform_response(parsed_response)
 
-#{'timestamp': '2019-06-19', 'open': 1105.6, 'high': 1107.0, 'low': 1093.48, 'close': 1102.33, 'volume': 1338575}
-
-def to_usd(my_price):
-    # utility function to convert float or integer to usd-formatted string (for printing)
-    return "${0:,.2f}".format(my_price) #> $12,000.71
-
 latest_day = tsd[0]['timestamp']
 last_close = tsd[0]['close']
 high =[]
 low =[]
 
-i=0
-while (i<100):
-    
-    for p in tsd:
-        high.append(p['high'])
-        low.append(p['low'])
-        break
-    i=i+1
-breakpoint()
+for p in tsd:
+    high.append(p['high'])
+    low.append(p['low'])
 recent_high = max(high)
 recent_low = min(low)
 
+q=[]
+q = df.DataFrame(tsd)
+if last_close > df.q.quantile(0.9):
+    print("go for it!")
+
+
+
+csv_filepath = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+write_to_csv(tsd, csv_filepath)
 
 
 print("-------------------------")
